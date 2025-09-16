38ff66c776df5ee3f3672a203ed55a9d4da4982f
aravinthk <aravinthk1809@gmail.com>
2025-09-16 20:46:37 +0530

    - strictly app start with login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'providers/auth_provider.dart';
 import 'views/home_screen.dart';
 import 'views/login_screen.dart';
 import 'views/signup_screen.dart';
@@ -14,14 +13,12 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authState = ref.watch(authProvider);
+    
 
     return MaterialApp(
       title: 'Bytelogik App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: authState.status == AuthStatus.authenticated
-          ? const HomePage()
-          : const LoginPage(),
+      home: const LoginPage(),
       routes: {
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const SignupPage(),
